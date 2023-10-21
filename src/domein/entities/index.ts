f095e5e0cef737e012759ec3cc6e4c22e8a8dc24
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-10-21 21:42:53 +0400

    added entities to index file

--- src/domein/entities/index.ts
+++ src/domein/entities/index.ts
@@ -1,5 +1,10 @@
 /* eslint-disable prettier/prettier */
-const entities = {};
+import { Users } from './users.entity';
+import { Blogs } from './blogs.entity';
+import { Comments } from './comments.entity';
+import { Roles } from './roles.entity';
+import { Tags } from './tags.entity';
+const entities = { Users, Blogs, Comments, Roles, Tags };
 
-export {};
+export { Users, Blogs, Comments, Roles, Tags };
 export default entities;
