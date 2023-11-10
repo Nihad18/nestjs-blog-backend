c7384c667215dca1db249dbc9d55a1104e93758e
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-11-10 20:56:25 +0400

    refactor: reorganized code for readability

--- src/infrastructure/modules/app.module.ts
+++ src/infrastructure/modules/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+// modules
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// custom modules
 import { AuthModule } from 'src/domein/modules/auth/auth.module';
 import { BlogModule } from 'src/domein/modules/blogs/blogs.module';
 import { UsersModule } from 'src/domein/modules/users/users.module';
+//config files
 import databaseConfig from '../config/typeorm.config';
-
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
