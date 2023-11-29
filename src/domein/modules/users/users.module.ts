a9d6ec282f1e14314f8132eb8a0e5601c6c6758e
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-11-30 00:33:43 +0400

    feat(middleware): add UuidValidationMiddleware to user routes

--- src/domein/modules/users/users.module.ts
+++ src/domein/modules/users/users.module.ts
@@ -1,14 +1,28 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from 'src/application/controllers/users/users.controller';
-import { Roles, Users } from 'src/domein/entities';
+import { Blogs, Roles, Users } from 'src/domein/entities';
+import { UuidValidationMiddleware } from 'src/domein/middlewares/uuidvalidation.middleware';
 import { UsersService } from 'src/domein/services/users/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users,Roles])],
+  imports: [TypeOrmModule.forFeature([Users, Roles,Blogs])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(UuidValidationMiddleware)
+      .exclude({
+        path: 'users/',
+        method: RequestMethod.GET,
+      })
+      .forRoutes(
+        { path: 'users/:id', method: RequestMethod.ALL },
+        { path: 'users/change-password/:id', method: RequestMethod.PATCH },
+      );
+  }
+}
