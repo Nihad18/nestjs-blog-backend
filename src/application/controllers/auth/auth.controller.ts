ff378b49d246a28668abc5135024e36e59d9e16c
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-12-02 00:01:34 +0400

    feat(controller): added login, registration, OTP sending, and account activation endpoints

--- src/application/controllers/auth/auth.controller.ts
+++ src/application/controllers/auth/auth.controller.ts
@@ -1,21 +1,56 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Res,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthRequestDto } from 'src/application/dtos/auth/auth.request.dto';
+import { AuthResponseDto } from 'src/application/dtos/auth/auth.response.dto';
+import { UserRequestDto } from 'src/application/dtos/users/user.request.dto';
 import { AuthService } from 'src/domein/services/auth/auth.service';
-
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
+  @UsePipes(ValidationPipe)
   async login(
     @Res() res,
     @Body() authRequestDto: AuthRequestDto,
-  ): Promise<any> {
+  ): Promise<AuthResponseDto> {
+    const response = await this.authService.authenticate(authRequestDto);
+    return response;
+  }
+
+  @Post('/register')
+  @UsePipes(ValidationPipe)
+  async register(
+    @Body() userRequestDto: UserRequestDto,
+  ): Promise<void | object> {
+    return this.authService.createUser(userRequestDto);
+  }
+
+  @Post('/send-otp')
+  async sendOtp(@Body('email') email: string) {
     try {
-      const response = await this.authService.authenticate(authRequestDto);
-      return res.status(HttpStatus.OK).json(response);
+      const response = await this.authService.sendOtp(email);
+      return response;
     } catch (error) {
-      return res.status(error.status).json({ error });
+      return { message: 'Failed to send email', error: error.message };
+    }
+  }
+  @Post('activate-account')
+  async activateAccount(
+    @Body('email') email: string,
+    @Body('otpCode') otpCode: string,
+  ) {
+    try {
+      const response = await this.authService.activateAccount(email, otpCode);
+      return response;
+    } catch (error) {
+      return { message: 'Failed to send email', error: error.message };
     }
   }
 }
