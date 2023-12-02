31699b564d4657a77ed23b158010e03ef2b3ca08
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2023-12-03 00:10:19 +0400

    feat(module): added BlogModule with TypeOrm integration

--- src/domein/modules/blogs/blogs.module.ts
+++ src/domein/modules/blogs/blogs.module.ts
@@ -1,4 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+// modules
+import { TypeOrmModule } from '@nestjs/typeorm';
+// controllers
+import { BlogsController } from 'src/application/controllers/blogs/blogs.controller';
+// entities
+import { Blogs, Roles, Users } from 'src/domein/entities';
+// services
+import { BlogsService } from 'src/domein/services/blogs/blogs.service';
 
-@Module({})
-export class BlogModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([Users, Roles, Blogs])],
+  providers: [BlogsService],
+  controllers: [BlogsController],
+})
+export class BlogModule {
+}
